3c40e8326a92e8c68a2e05d9c0347ea817411a55
Dhruv-Garodia <79439205+Dhruv-Garodia@users.noreply.github.com>
2024-04-02 09:27:27 +0530

    Time: 2187 ms (7.05%), Space: 6.9 MB (100%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings_2.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+     if(s.size()!=t.size())
+        return false;
+    else
+    {
+        for(int i=0;i<s.size();i++)
+        {
+            for(int j=i+1;j<s.size();j++)
+            {
+                if(s[i]==s[j])
+                {
+                    if(t[i]!=t[j])
+                        return false;
+                }
+                else
+                {
+                    if(t[i]==t[j])
+                        return false;
+                }
+            }
+        }
+
+    }
+    return true;   
+    }
+};
